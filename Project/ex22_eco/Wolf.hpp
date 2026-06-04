21e3a9aa7cc51ca88325f84143b3ebf386c8cf37
fgeslin <flo.ges.8@gmail.com>
2026-06-04 21:08:43 +0900

    cpp project v1

---
+++ Project/ex22_eco/Wolf.hpp
@@ -0,0 +1,17 @@
+#ifndef WOLF_HPP
+#define WOLF_HPP
+
+#include <iostream>
+#include <string>
+#include "Entity.hpp"
+
+class Wolf : public Entity
+{
+private:
+	/* data */
+public:
+	Wolf(/* args */);
+	~Wolf();
+};
+
+#endif
